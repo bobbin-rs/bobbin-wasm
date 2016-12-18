fc96e623f9850ab3a55bb637391b5d31d8dec92f
Jonathan Soo <jcsoo@agora.com>
2016-12-18 17:47:52 -0500

    1st try at big state machine

--- wasm-reader/src/lib.rs
+++ wasm-reader/src/lib.rs
@@ -4,8 +4,96 @@ extern crate wasm_leb128;
 extern crate byteorder;
 
 use wasm_leb128::{read_u7, read_u32};
-
 use byteorder::{ByteOrder, LittleEndian};
+
+#[derive(Debug)]
+pub enum Error {
+    BufferTooShort,
+    Unspecified,
+}
+
+impl From<wasm_leb128::Error> for Error {
+    fn from(_other: wasm_leb128::Error) -> Error {
+        Error::Unspecified
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ReaderState {
+    MagicNumber,
+    Version,
+    SectionId,
+    SectionPayloadLen,
+    Done,
+    Error,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ReaderEvent {
+    MagicNumber(u32),
+    Version(u32),
+    SectionId(u8),
+    SectionPayloadLen(u32),
+    Done,
+    Error,
+}
+
+pub struct Reader<'a> {
+    buf: &'a [u8],
+    pos: usize,
+    state: ReaderState,
+}
+
+impl<'a> Reader<'a> {
+    pub fn new(buf: &'a [u8]) -> Self {
+        Reader { buf: buf, pos: 0, state: ReaderState::MagicNumber }
+    }
+
+    pub fn remaining(&self) -> usize {
+        self.buf.len() - self.pos
+    }
+
+    pub fn slice_of(&self, size: usize) -> Result<&'a [u8], Error> {
+        if size < self.remaining() { 
+            Ok(&self.buf[self.pos..self.pos+4])
+        } else {
+            Err(Error::BufferTooShort)
+        }
+    }
+
+    pub fn read_u32(&mut self) -> Result<u32, Error> {
+        let v = LittleEndian::read_u32(try!(self.slice_of(4)));
+        self.pos += 4;
+        Ok(v)
+    }
+
+    pub fn read_var_u7(&mut self) -> Result<u8, Error> {
+        let (v, n) = try!(read_u7(&self.buf[self.pos..]));
+        self.pos += n;
+        Ok(v)
+    }
+
+    pub fn read_var_u32(&mut self) -> Result<u32, Error> {
+        let (v, n) = try!(read_u32(&self.buf[self.pos..]));
+        self.pos += n;
+        Ok(v)
+    }
+
+    pub fn next(&mut self) -> Result<ReaderEvent, Error> {
+        use ReaderState::*;
+        let (event, state) = match self.state {
+            MagicNumber => (ReaderEvent::MagicNumber(try!(self.read_u32())), Version),
+            Version => (ReaderEvent::Version(try!(self.read_u32())), SectionId),            
+            SectionId => (ReaderEvent::SectionId(try!(self.read_var_u7())), SectionPayloadLen),
+            SectionPayloadLen => (ReaderEvent::Done, Done),
+            Done => (ReaderEvent::Done, Done),
+            Error => (ReaderEvent::Error, Error)
+        };
+        self.state = state;
+        Ok(event)
+    }
+}
+
 
 pub struct ModuleReader<'a> {
     buf: &'a [u8],
@@ -54,16 +142,12 @@ mod tests {
     const BASIC: &'static [u8] = include_bytes!("../testdata/basic.wasm");
 
     #[test]
-    fn test_module() {
-        let r = ModuleReader::new(BASIC);
-        assert_eq!(r.magic(), 0x6d736100);
-        assert_eq!(r.version(), 0x0d);
-    }
+    fn test_reader() {
+        use self::ReaderEvent::*;
+        let mut r = Reader::new(BASIC);
+        assert_eq!(r.next().unwrap(), MagicNumber(0x6d736100));
+        assert_eq!(r.next().unwrap(), Version(0xd));
+        assert_eq!(r.next().unwrap(), SectionId(1));
 
-    #[test]
-    fn test_section() {
-        let s = SectionReader::new(&BASIC[8..]);
-        assert_eq!(s.id(), 1);
-        assert_eq!(s.payload_len(), 1);
     }
 }
