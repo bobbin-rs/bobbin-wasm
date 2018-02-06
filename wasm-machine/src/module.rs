77b67434d550b480ada038331e54d4aac294ade5
Jonathan Soo <jcsoo@agora.com>
2018-02-06 16:51:09 -0500

    Start implementing module accessors

--- wasm-machine/src/module.rs
+++ wasm-machine/src/module.rs
@@ -2,11 +2,17 @@ use {Reader, SectionType};
 
 pub struct Module<'a> {
     r: Reader<'a>,
+    types: [Option<(u32, u32)>; 64],
+    funcs: [Option<u32>; 64],
 }
 
 impl<'a> Module<'a> {
     pub fn new(r: Reader<'a>) -> Self {
-        Module { r }
+        Module { 
+            r,
+            types: [None; 64],
+            funcs: [None; 64],
+        }
     }
 
     pub fn read_u8_at(&self, offset: usize) -> u8 {
@@ -24,14 +30,6 @@ impl<'a> Module<'a> {
     pub fn join_reader(&mut self, r: Reader) {
         self.r.join_reader(r)
     }
-}
-
-pub struct Section<'a> {
-    m: &'a Module<'a>,
-    pub off: u32,
-    pub sid: SectionType,
-    pub len: u32,
-    pub cnt: u32,
 }
 
 pub struct SectionIter<'a> {
@@ -55,3 +53,66 @@ impl<'a> Iterator for SectionIter<'a> {
         }
     }
 }
+
+pub struct Section<'a> {
+    m: &'a Module<'a>,
+    pub off: u32,
+    pub sid: SectionType,
+    pub len: u32,
+    pub cnt: u32,
+}
+
+impl<'a> Section<'a> {
+    pub fn iter_types(&'a self) -> TypeIter<'a> {
+        assert!(self.sid == SectionType::Type);
+        TypeIter { m: self.m, s: self, pos: self.off, n: 0 }
+    }
+
+    // pub fn iter_functions(&'a self) -> FunctionIter<'a> {
+    //     assert!(self.sid == SectionType::Type);
+    //     FunctionIter { m: self, s: self, pos: self.off }
+    // }    
+}
+
+
+pub struct TypeIter<'a> {
+    m: &'a Module<'a>,
+    s: &'a Section<'a>,
+    pos: u32,
+    n: u32,
+}
+
+impl<'a> Iterator for TypeIter<'a> {
+    type Item = Type<'a>;
+    fn next(&mut self) -> Option<Type<'a>> {
+        if self.n < self.s.cnt {           
+            let pos = self.pos as u32;
+            let n = self.n;            
+            let len = self.m.read_u32_at(self.pos as usize);
+            self.n += 1;
+            self.pos += len;
+            Some(Type{ m: self.m, s: self.s, pos, n })
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Type<'a> {
+    m: &'a Module<'a>,
+    s: &'a Section<'a>,
+    pos: u32,
+    n: u32,    
+}
+
+// impl<'a> Type<'a> {
+//     pub fn params(&self) -> &[u8] {
+
+//     }
+// }
+
+// pub struct FunctionIter<'a> {
+//     m: &'a Module<'a>,
+//     pos: usize,
+//     s: &'a Section<'a>,    
+// }
