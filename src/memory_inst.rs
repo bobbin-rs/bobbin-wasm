9d07035195f77d4941c6a4f18fb77250eab7fd62
Jonathan Soo <jcsoo@agora.com>
2018-02-12 15:37:26 -0500

    Add memory instructions

--- src/memory_inst.rs
+++ src/memory_inst.rs
@@ -60,21 +60,63 @@ impl<'a> MemoryInst<'a> {
         unsafe { slice::from_raw_parts_mut(self.buf, self.len.get() * PAGE_SIZE) }
     }
 
-    fn check_access(&self, index: usize) -> Result<(), Error> {
-        if index < self.len() { Ok(()) } else { Err(Error::OutOfBounds) }
+    fn check_access(&self, index: usize, len: usize) -> Result<(), Error> {
+        if index + len <= self.len() { Ok(()) } else { Err(Error::OutOfBounds) }
     }
 
-    pub fn load(&self, index: usize) -> Result<i32, Error> {
+    pub fn load_32(&self, index: usize) -> Result<i32, Error> {
         Ok({
-            self.check_access(index)?;
+            self.check_access(index, 4)?;
             LittleEndian::read_i32(&self.as_ref()[index..])
         })
     }
 
-    pub fn store(&self, index: usize, value: i32) -> Result<(), Error> {
+    pub fn load_16_s(&self, index: usize) -> Result<i32, Error> {
         Ok({
-            self.check_access(index)?;
+            self.check_access(index, 2)?;
+            LittleEndian::read_i16(&self.as_ref()[index..]) as i16 as i32
+        })
+    }
+
+    pub fn load_16_u(&self, index: usize) -> Result<i32, Error> {
+        Ok({
+            self.check_access(index, 2)?;
+            LittleEndian::read_i16(&self.as_ref()[index..]) as u16 as i32
+        })
+    }
+
+
+    pub fn load8_u(&self, index: usize) -> Result<(), Error> {
+        Ok({
+            self.check_access(index, 1)?;
+            self.as_mut()[index] as u8 as i32;
+        })
+    }    
+
+    pub fn load8_s(&self, index: usize) -> Result<(), Error> {
+        Ok({
+            self.check_access(index, 1)?;
+            self.as_mut()[index] as i8 as i32;
+        })
+    }    
+
+    pub fn store_32(&self, index: usize, value: i32) -> Result<(), Error> {
+        Ok({
+            self.check_access(index, 4)?;
             LittleEndian::write_i32(&mut self.as_mut()[index..], value)
+        })
+    }
+
+    pub fn store_16(&self, index: usize, value: i16) -> Result<(), Error> {
+        Ok({
+            self.check_access(index, 2)?;
+            LittleEndian::write_i16(&mut self.as_mut()[index..], value)
+        })
+    }
+    pub fn store_8(&self, index: usize, value: i8) -> Result<(), Error> {
+        Ok({
+            self.check_access(index, 1)?;
+            self.as_mut()[index] = value as u8;
         })
     }
 
@@ -90,11 +132,11 @@ mod tests {
         let mem = MemoryInst::new(&mut buf, 1, Some(4));
 
         for i in 0..4 {
-            mem.store(i * 4, i as i32).unwrap();
+            mem.store_32(i * 4, i as i32).unwrap();
         }
 
         for i in 0..4 {
-            assert_eq!(mem.load(i * 4).unwrap(), i as i32);
+            assert_eq!(mem.load_32(i * 4).unwrap(), i as i32);
         }
 
     }
