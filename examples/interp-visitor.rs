a200439643b4af7ba53fa756ecf692593c822a26
Jonathan Soo <jcsoo@agora.com>
2018-02-16 13:44:26 -0500

    Remove non inplace module

--- examples/interp-visitor.rs
+++ examples/interp-visitor.rs
@@ -63,38 +63,28 @@ pub fn run(matches: ArgMatches) -> Result<(), Error> {
 
     let _out = String::new();
 
-
-    let mut module_buf = [0u8; 4096];
     let m = Module::from(data.as_ref());
 
-    let cfg = wasm::loader::Config::default();
-    let mut loader = wasm::loader::Loader::new_with_config(cfg, &mut module_buf[..]);
-    visitor::visit(&m, &mut loader)?;
-
-    let (lm, buf) = loader.module();
-
-
     let mut code_buf = [0u8; 4096];
+
+
     let cfg = wasm::compiler::Config::default();
     let mut compiler = wasm::compiler::Compiler::new_with_config(cfg, &mut code_buf[..]);
     let code = compiler.compile(&m)?;
-    println!("CODE");
-    for (i, b) in code.iter().enumerate() {
-        println!("  {}: {:08x} to {:08x}", i, b.start, b.end);
-    }
 
-    if matches.is_present("dump") {
-        print!("{:?}", lm);
-    }
+    // println!("CODE");
+    // for (i, b) in code.iter().enumerate() {
+    //     println!("  {}: {:08x} to {:08x}", i, b.start, b.end);
+    // }
 
     let mut memory_buf = [0u8; 256];
     let memory = MemoryInst::new(&mut memory_buf, 1, None);
 
     // let (mi, _buf) = lm.instantiate(buf, &memory)?;
 
+    let mut buf = [0u8; 2048];
 
-    let (mi, _buf) = m.instantiate(buf, &memory, &code)?;
-
+    let (mi, _buf) = m.instantiate(&mut buf, &memory, &code)?;
 
     // Interpreter
 
@@ -104,43 +94,37 @@ pub fn run(matches: ArgMatches) -> Result<(), Error> {
 
     let mut interp = inplace::interp::Interp::new(&mut buf);
 
-    for s in lm.iter() {
-        if s.section_type == SectionType::Export {
-            for e in s.exports() {
+    if let Some(export_section) = m.export_section() {
+        for e in export_section.iter() {
+            if let ExportDesc::Function(index) = e.export_desc {
+                let t = m.function_signature_type(index as usize).unwrap();
+                let id = std::str::from_utf8(e.identifier.0).unwrap();
+                match interp.run(&mi, index as usize) {
+                    Ok(_) => {
+                        if t.returns().count() == 1 {
+                            if interp.stack_len() == 1 {
+                                println!("{}() => {}:{}", id, TypeValue::try_from(t.returns().nth(0).unwrap()).unwrap(), interp.pop().unwrap() as u32);
+                            } else {
+                                println!("---- Stack Dump ----");
 
-                if let ExportDesc::Function(index) = e.export_desc {
-                    let t = lm.function_signature_type(index).unwrap();
-                    let id = std::str::from_utf8(e.identifier.0).unwrap();
-                    match interp.run(&mi, index as usize) {
-                        Ok(_) => {
-                            if t.returns.len() == 1 {
-                                if interp.stack_len() == 1 {
-                                    println!("{}() => {}:{}", id, TypeValue::try_from(t.returns[0]).unwrap(), interp.pop().unwrap() as u32);
-                                } else {
-                                    println!("---- Stack Dump ----");
-
-                                    let mut i = 0;
-                                    while let Ok(value) = interp.pop() {
-                                        println!("{}: {:?}", i, value);
-                                        i += 1;
-                                    }
-                                    println!("---- END ----");
+                                let mut i = 0;
+                                while let Ok(value) = interp.pop() {
+                                    println!("{}: {:?}", i, value);
+                                    i += 1;
                                 }
+                                println!("---- END ----");
                             }
-                        }, 
-                        Err(err) => {
-                            println!("{}() => {:?}", id, err);
                         }
+                    }, 
+                    Err(err) => {
+                        println!("{}() => {:?}", id, err);
                     }
-
-
                 }
+
+
             }
-            
         }
     }
-
-
     
     Ok(())
 }
