20d9905e59a8cd419d6a685694c8414ab625ff8d
Jonathan Soo <jcsoo@agora.com>
2018-05-08 17:56:50 -0400

    Add more complex example

--- wasm-blinky/src/lib.rs
+++ wasm-blinky/src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![feature(wasm_import_module)]
 
+const DIGITS: &[u8; 16] = b"0123456789abcdef";    
+
 mod host {
     #[wasm_import_module="host"]
     extern {
@@ -10,12 +12,35 @@ mod host {
     }
 }
 
+fn u8_to_hex(c: u8) -> [u8; 2] {
+    [DIGITS[((c >> 4) & 0xf) as usize], DIGITS[(c & 0xf) as usize]]
+}
+
+fn u16_to_hex(c: u16) -> [u8; 4] {
+    let (a, b) = (u8_to_hex((c >> 8) as u8), u8_to_hex(c as u8));
+    [a[0], a[1], b[0], b[1]]
+}
+
+fn u32_to_hex(c: u32) -> [u8; 8] {
+    let (a, b) = (u16_to_hex((c >> 16) as u16), u16_to_hex(c as u16));
+    [a[0], a[1], a[2], a[3], b[0], b[1], b[2], b[3]]
+}
+
 fn write(buf: &[u8]) {
     unsafe { host::write(buf.as_ptr(), buf.len() as i32) };
 }
 
 fn write_str(buf: &str) {
     write(buf.as_bytes())
+}
+
+fn write_u8(value: u8) {
+    write(u8_to_hex(value).as_ref())
+}
+
+fn write_hex(value: u32) {
+    let buf = u32_to_hex(value);
+    write(buf.as_ref());
 }
 
 fn led(state: bool) {
@@ -28,11 +53,14 @@ fn delay(ms: u32) {
 
 #[no_mangle]
 pub extern "C" fn main() {
+    let mut i = 0x10u8;
     loop {
-        write_str("Hello, World\n");
+        write_u8(i);
+        write_str("\n");
         led(true);
         delay(500);
         led(false);
         delay(500);
+        // i = i.wrapping_add(1);
     }
 }
