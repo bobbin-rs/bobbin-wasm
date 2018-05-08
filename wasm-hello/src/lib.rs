1218a5ad45092d80199b759a7e05b2409265ad33
Jonathan Soo <jcsoo@agora.com>
2018-05-08 14:32:28 -0400

    Use wasm_import_module in example

--- wasm-hello/src/lib.rs
+++ wasm-hello/src/lib.rs
@@ -1,11 +1,9 @@
+#![feature(wasm_import_module)]
+
+#[wasm_import_module="host"]
 extern {
     fn hello();
 }
-
-// #[no_mangle]
-// extern "C" fn add_one(x: i32) -> i32 {
-//     x + 1
-// }
 
 #[no_mangle]
 pub extern "C" fn run_hello() {
